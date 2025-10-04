11f5e203f6f216e86be35045d0211a233e1be80a
Tanush Arora <tanush935@gmail.com>
2025-10-04 16:54:22 +0530

    database connected

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-mongoose.connect("mongodb+srv://namastetanush:yeRNokl3BlM7tcnu@namastenode.qikrg8y.mongodb.net/devTinder");
+await mongoose.connect(
+    "mongodb+srv://namastetanush:yeRNokl3BlM7tcnu@namastenode.qikrg8y.mongodb.net/devTinder"
+);
 };
 
 module.exports = connectDB;
